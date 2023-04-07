d67ad7732bd0b156e7ee23ffaa4ffe81aa23e079
Ajay Shridhar Joshi <ajsj7598@gmail.com>
2023-04-07 16:07:31 -0500

    Add support for variable size in KV-store (#2)

--- Project/kv-store/client.cpp
+++ Project/kv-store/client.cpp
@@ -13,37 +13,29 @@ uint64_t get_current_time() {
 	return tv.tv_sec * 1000000 + tv.tv_usec;
 }
 
-struct kv_request generate_request() {
-    struct kv_request req;
-    req.type = rand() % 2;
-    req.key = rand() % 100;
-    if (req.type == 1) { // put
-        req.value = rand() % 100;
-    }
-    return req;
-}
-
-
 void send_data(int sockfd) {
     char buff[MAXLINE];
     int buffer_size = MAXLINE;
 
-	uint64_t total_time = 0, iters = 1024 * 256, total_bytes_sent = 0;
+	char* req = (char*)malloc(MAXLINE);
+	uint64_t total_time = 0, iters = 1024 * 32, total_bytes_sent = 0;
 
 	for(int i = 0; i < iters; i++) {
 		// send data
 		uint64_t start, end;
 
-        struct kv_request req = generate_request();
-		if(i == iters - 1) req.type = -1; // exit signal
+        bool final = false;
+		if(i == iters - 1) final = true;
+		uint64_t len = generate_request(req, final);
 
 		start = get_current_time();
-		ssize_t sent_bytes = rwrite(sockfd, &req, sizeof(req));
-		int read_bytes = rread(sockfd, buff, buffer_size);
-		uint64_t* resp = (uint64_t*)buff;
+		ssize_t sent_bytes = write(sockfd, req, len);
+
+		int read_bytes = read(sockfd, buff, buffer_size);
 		end = get_current_time();
 		total_time += end - start;
 		total_bytes_sent += sent_bytes;
+		// cout << "Response: " << string(buff) << endl;
 		// assert(read_bytes == 4);
 		// assert(strcmp(buff, "ack\0") == 0);
 		// printf("Sent + ack time: %lu\n", end - start);
@@ -62,7 +54,7 @@ int main()
 	struct sockaddr_in servaddr, cli;
 
 	// socket create and verification
-	sockfd = rsocket(AF_INET, SOCK_STREAM, 0);
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd == -1) {
 		printf("socket creation failed...\n");
 		exit(0);
@@ -79,7 +71,7 @@ int main()
 	servaddr.sin_port = htons(SERV_PORT);
 
 	// connect the client socket to server socket
-	if (rconnect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) {
+	if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) {
 		printf("connection with the server failed...\n");
 		exit(0);
 	}
@@ -90,5 +82,5 @@ int main()
 	send_data(sockfd);
 
 	// close the socket
-	rclose(sockfd);
+	close(sockfd);
 }
