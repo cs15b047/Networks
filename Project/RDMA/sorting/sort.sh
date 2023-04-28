90b5df3e7734828b883d90c1161f7386fc492291
Ajay Joshi <ajsj7598@gmail.com>
2023-04-28 14:29:52 -0600

    Fix integer overflow issues

--- Project/RDMA/sorting/sort.sh
+++ Project/RDMA/sorting/sort.sh
@@ -1,10 +1,12 @@
-make clean && make
+make
 
-workers=$1
-N=$2
-master_ip=$3
+rank_start=$1
+rank_end=$2
+workers=$3
+N=$4
+master_ip=$5
 
-for ((i=1; i<$workers; i++))
+for ((i=$rank_start; i<=$rank_end; i++))
 do
     ./sort $workers $N $master_ip $i &
 done
