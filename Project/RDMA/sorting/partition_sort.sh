6d9c5f0ce8cef4301756bf83daaa9e2f939f31dc
sarthi <sarthi@node-2.intel-nic-740.cs740-uwmadison-pg0.utah.cloudlab.us>
2023-05-03 13:45:54 -0600

    Add distributed sorting with sockets

--- Project/RDMA/sorting/partition_sort.sh
+++ Project/RDMA/sorting/partition_sort.sh
@@ -1,16 +1,25 @@
-make clean && make
+make clean && make all
 
 workers=$1
 N=$2
 num_servers=$3
 ip=$4
-server_id=$5
+type=$5
+server_id=$6
+
+if $type -eq "rdma"
+then
+    prog="rdma_sort"
+else
+    prog="kernel_sort"
+fi
+
 # Alternate between the two IPs
 for ((i=0; i<$workers; i++))
 do
     # Run if the server ID is the same as the rank % num_servers
     if [ $((i % num_servers)) -eq $server_id ]
     then
-        ./sort $workers $N $ip $i &
+        ./$prog $workers $N $ip $i &
     fi
 done
